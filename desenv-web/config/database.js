9ecdfe64fd98ca4c5daa4919291d8a54b598e009
Mateus Euzébio <mateuseuzebio@hotmail.com>
2024-08-18 19:27:31 -0300

    atualização completa

---
+++ desenv-web/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'm4t3l5',
+    database: 'api-node',
+    define: {
+        timestamps: true,
+        underscored: false
+    }
+}
---
+++ desenv-web/migrations/20240812231609-create-endereço.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { Sequelize } = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('enderecos', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Cep: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Logradouro: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      Complemento: {
+        type: Sequelize.STRING,
+      },
+      Bairro: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Cidade: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Estado: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      MunicipioIBGE: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('enderecos')
+  }
+};
